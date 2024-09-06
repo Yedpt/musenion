09901447b6232872ebf5efda912cafc5b12e4ad4
marie-adi <marie.adim@gmail.com>
2024-09-06 18:31:22 +0200

    add text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,11 @@ const Home = () => {
 
       <section id="section2">
         <h2>Sobre el museo</h2>
-        <p>Texto</p>
+        <p>¡Bienvenidos al Museo Virtual Musenion! 
+Te invitamos a descubrir un espacio único donde el humor y la creatividad se dan la mano para ofrecer una experiencia digital inolvidable. 
+¡Tú también puedes ser parte del museo! Anímate a subir tus propios memes de Minions y competir por un lugar en nuestra prestigiosa galería, tu creación podría ser la próxima obra destacada de nuestra colección permanente. No hay límites para la imaginación, y cada meme tiene su oportunidad de convertirse en una obra maestra.
+Musenion te espera para que explores, te diviertas y participes activamente en esta comunidad.
+¡No te lo pierdas, crea, comparte y sé parte de la historia del museo! </p>
       </section>
 
     {/*   <Footer /> */}
