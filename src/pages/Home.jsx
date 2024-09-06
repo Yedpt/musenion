0426dbd295b8ad56da405321487d18a64a930339
marie-adi <marie.adim@gmail.com>
2024-09-06 12:52:55 +0200

    cambios home y assets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      hola
-    </div>
+    <>
+      <Nav />
+
+      <section id="section1">
+        <img src="" alt="" />
+        <h1>Musenion</h1>
+      </section>
+
+      <section id="section2">
+        <h2>Sobre el museo</h2>
+        <p>Este es el contenido de la segunda sección.</p>
+      </section>
+
+      <Footer />
+    </>
   )
 }
 
