448d9c60a26486520d9166b1c359fd89f6de90da
devegalaura-dev <lauradevega94@gmail.com>
2024-09-09 09:06:19 +0200

    Add style to form

--- src/pages/MemeDetail.jsx
+++ src/pages/MemeDetail.jsx
@@ -12,84 +12,137 @@ function Form() {
 
   });
 
+  const PageContainer = styled.div`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+      background-color: #e9ecef;
+  `;
+
   const FormContainer = styled.form`
-   
+    display: flex;
+    flex-direction: column;
+    max-width: 500px;
+    margin: auto;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    background-color: white;
   `;
   const Label = styled.label`
-  
+  margin-bottom: 8px;
+  font-weight: bold;
+  color: black;
   `;
-  const Input = styled.input``;
-  const SubmitButton = styled.button``;
+  const Input = styled.input`
+  margin-bottom: 12px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  background: white;
+  color: gray;
+
+  &:focus {
+    border-color: yellow;
+    outline: none;
+  }
+  `;
+  const TextArea = styled.textarea`
+  margin-bottom: 12px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  background: white;
+  color: gray;
+
+  &:focus {
+    border-color: yellow;
+    outline: none;
+  }
+  `;
+  const SubmitButton = styled.button`
+    
+  `;
+  const Advise = styled.span`
+  color: red;
+  `;
+
   
   return (
-    <FormContainer onSubmit={onSubmit}>
+    <PageContainer>
+        <FormContainer onSubmit={onSubmit}>
 
-      <Label htmlFor="nombre">Nombre</Label>
-      <Input 
-      type="text"
-       id="nombre" 
-       {...register("nombre",{
-        required: true
-       })
-    } />
-    {
-      errors.nombre && <span>Espacio requerido.</span>
-    }
-    
-    <Label htmlFor="surname">Apellidos</Label>
-      <Input 
-      type="text"
-       id="surname" 
-       {...register("surname",{
-        required: true
-       })
-    } />
-    {
-      errors.surname && <span>Espacio requerido.</span>
-    }
-
-
-      <Label htmlFor="correo">Correo</Label>
-      <Input type="email" id="correo" {...register("correo", {
-        required: {
-          value: true,
-          message: "El correo es un campo requerido."
-        },
-        pattern: {
-          value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/, /*expresión regular*/
-          message: "El correo no es válido."
+          <Label htmlFor="nombre">Nombre</Label>
+          <Input 
+          type="text"
+          id="nombre" 
+          {...register("nombre",{
+            required: true
+          })
+        } />
+        {
+          errors.nombre && <Advise>Espacio requerido.</Advise>
+        }
+        
+        <Label htmlFor="surname">Apellidos</Label>
+          <Input 
+          type="text"
+          id="surname" 
+          {...register("surname",{
+            required: true
+          })
+        } />
+        {
+          errors.surname && <Advise>Espacio requerido.</Advise>
         }
 
-      })} />
 
-      {
-        errors.correo && <span>{errors.correo.message}</span>
-      }
+          <Label htmlFor="correo">Correo</Label>
+          <Input type="email" id="correo" {...register("correo", {
+            required: {
+              value: true,
+              message: "El correo es un campo requerido."
+            },
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/, /*expresión regular*/
+              message: "El correo no es válido."
+            }
 
-      <Label htmlFor="message">Mensaje</Label>
-      <Input 
-      type="text"
-       id="message" 
-       {...register("message",{
-        required: true
-       })
-    } />
-    {
-      errors.message && <span>Espacio requerido.</span>
-    }
+          })} />
 
-    <Label htmlFor="files">📎</Label>
-    <Input
-      type="file"
-      id="files"
-      {...register("files")}
-    />
+          {
+            errors.correo && <Advise>{errors.correo.message}</Advise>
+          }
 
-      <Label htmlFor="terms">Acepto términos y condiciones.</Label>
-      <Input type="checkbox" id="terms" {...register("terms")} />
+          <Label htmlFor="message">Mensaje</Label>
+          <TextArea
+          type="text"
+          id="message" 
+          {...register("message",{
+            required: true
+          })
+        } />
+        {
+          errors.message && <Advise>Espacio requerido.</Advise>
+        }
 
-      <SubmitButton type="submit">Enviar</SubmitButton>
-    </FormContainer>
+        <Label htmlFor="files">📎</Label>
+        <Input
+          type="file"
+          id="files"
+          {...register("files")}
+        />
+
+          <Label htmlFor="terms">Acepto términos y condiciones.</Label>
+          <Input type="checkbox" id="terms" {...register("terms")} />
+
+          <SubmitButton type="submit">Enviar</SubmitButton>
+        </FormContainer>
+    </PageContainer>
     
   )
 }
