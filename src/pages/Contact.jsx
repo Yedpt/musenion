36d53540314bfbf9ad36fcbe3f9bf230cef874af
devegalaura-dev <lauradevega94@gmail.com>
2024-09-09 15:18:47 +0200

    Add SearchFiles, Conditions and AcceptButton const

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,108 @@
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
 
+const PageContainer = styled.body`
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      width: 100vw;   /* 100% del ancho de la ventana */
+      margin: 0;
+      background: linear-gradient(to bottom, #FFDC59, #E2730C);
+  `;
+
+  const TitlePage = styled.h2`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      color: #0C2849;
+      margin-top: 1px;
+      margin-bottom: 20px;
+      font-size: 24px;
+  `
+
+  const FormContainer = styled.form`
+    display: flex;
+    flex-direction: column;
+    max-width: 80%;
+    max-height: 90%;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    background-color: white;
+  `;
+
+  const Label = styled.label`
+  width: 90%;
+  margin-bottom: 8px;
+  font-weight: bold;
+  color: black;
+  font-size: 12px;
+  `;
+
+  const Input = styled.input`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 12px;
+  padding: 5px;
+  width: 90%;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  background: white;
+  color: gray;
+
+  &:focus {
+    border-color: yellow;
+    outline: none;
+  }
+  `;
+
+  const TextArea = styled.textarea`
+  width: 90%;
+  margin-bottom: 12px;
+  padding: 30px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  background: white;
+  color: gray;
+  font-size: 16px;
+
+  &:focus {
+    border-color: yellow;
+    outline: none;
+  }
+  `;
+
+  const SearchFiles = styled.input`
+  
+  `;
+
+  const Conditions = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    width: 90%;
+    padding: 10px;
+    font-size: 12px;
+  `;
+
+  const AcceptButton = styled.input`
+  
+  `;
+
+  const SubmitButton = styled.button`
+  width: 90%;
+  padding: 5px;
+  border-radius: 5px;
+  `;
+
+  const Advise = styled.span`
+  color: red;
+  `;
 
 function Contact() {
   const {register, handleSubmit, formState: {errors}} = useForm();
@@ -73,14 +175,15 @@ function Contact() {
         }
 
         <Label htmlFor="files">📎</Label>
-        <Input
+        <SearchFiles
           type="file"
           id="files"
           {...register("files")}
         />
-
+        <Conditions>
           <Label htmlFor="terms">Acepto términos y condiciones.</Label>
-          <Input type="checkbox" id="terms" {...register("terms")} />
+          <AcceptButton type="checkbox" id="terms" {...register("terms")} />
+        </Conditions>
 
           <SubmitButton type="submit">Enviar</SubmitButton>
         </FormContainer>   
@@ -88,92 +191,5 @@ function Contact() {
     
   )
 }
-
-  const PageContainer = styled.body`
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      width: 100vw;   /* 100% del ancho de la ventana */
-      margin: 0;
-      background: linear-gradient(to bottom, #FFDC59, #E2730C);
-  `;
-
-  const TitlePage = styled.h2`
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      flex-direction: column;
-      color: #0C2849;
-      margin-top: 1px;
-      margin-bottom: 20px;
-      font-size: 24px;
-  `
-
-  const FormContainer = styled.form`
-    display: flex;
-    flex-direction: column;
-    max-width: 60%;
-    max-height: 90%;
-    padding: 20px;
-    border-radius: 8px;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    background-color: white;
-  `;
-
-  const Label = styled.label`
-  margin-bottom: 8px;
-  font-weight: bold;
-  color: black;
-  font-size: 16px;
-  `;
-
-  const Input = styled.input`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 12px;
-  padding: 5px;
-  width: 90%;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  font-size: 16px;
-  background: white;
-  color: gray;
-
-  &:focus {
-    border-color: yellow;
-    outline: none;
-  }
-  `;
-
-  const TextArea = styled.textarea`
-  width: 70%;
-  margin-bottom: 12px;
-  padding: 30px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  font-size: 16px;
-  background: white;
-  color: gray;
-  font-size: 16px;
-
-  &:focus {
-    border-color: yellow;
-    outline: none;
-  }
-  `;
-
-  const SubmitButton = styled.button`
-  width: 90%;
-    
-  `;
-
-  const Advise = styled.span`
-  color: red;
-  `;
-
-  
-  
 
 export default Contact;
