3d7bc54e05acab0285a62fc153d375fd23736e04
a-bac-0 <anca.bacria@thevallians.com>
2024-09-12 10:39:16 +0200

    merge to update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,94 +1,141 @@
-import React, {useState} from 'react'
-import styled from 'styled-components'
-import BurgerButton from './BurgerButton'
+// import React, {useState} from 'react'
+// import styled from 'styled-components'
+// import BurgerButton from './BurgerButton'
+import { useState } from 'react'
+import  styles from './Nav';
 
-function Nav () {
 
-    const [clicked,setClicked] = useState(false)
-    const handleClick = () => {
-        //cuando true, lo transforma en false y viceversa
-        setClicked(!clicked)
+// function Nav () {
+
+//     const [clicked,setClicked] = useState(false)
+//     const handleClick = () => {
+//         //cuando true, lo transforma en false y viceversa
+//         setClicked(!clicked)
+//     }
+//     return (
+//         <>
+//         <NavContainer>
+//             <h2>Navbar <span>Responsive</span></h2>
+//             <ul className={`links ${clicked ? 'active': ''}`}>
+//                 <li><a href='/'>Home</a></li>
+//                 <li><a href='/'>Galeria Virtual</a></li>
+//                 <li><a href='/'>Sube tu Meme</a></li>
+//                 <li><a href='/'>Sobre Nosotros</a></li>
+//                 <li><a href='/'>Creadores</a></li>
+//                 <li><a href='/'>Contacto</a></li>
+//             </ul>
+//             <div className='burger'>
+//                 <BurgerButton clicked={clicked} handleClick={handleClick} />
+//             </div>
+            
+//         </NavContainer>
+//         </>
+//     )
+// }
+
+// export default Nav
+
+// const NavContainer = styled.nav`
+//     styled.h2{
+//         color: white;
+//         font-weight: 400;
+//         span{
+//             font-weight: bold;
+//         }
+//     }
+//     padding: .4rem;
+//     background-color: #333;
+//     display: flex;
+//     align-items: center;
+//     justify-content: space-between;
+//     a{
+//         color: white;
+//         text-text-decoration: none;
+//         margin-right: 1rem;
+//     }
+//     .links{
+//         position: absolute;
+//         top: -700px;
+//         left: -200px;
+//         margin-left: auto;
+//         margin-right: auto;
+//         text-align: center;
+//         a{
+//             color: black;
+//             font-size: 2rem;
+//             display: block;
+//         }
+//         @media(min-width: 768px){
+//             position: initial;
+//             margin: 0;
+//             a{
+//                 font-size: 1rem;
+//                 color: white;
+//                 display:inline;
+
+//             }
+//         }
+
+//     }
+
+//     .links.acive{
+//         width: 100%;
+//         display: block;
+//         position: absolute;
+//         margin-left: auto;
+//         margin-right: auto;
+//         top:30%;
+//         left: 0;
+//         right: 0;
+//         text-align: center;
+//     }
+//     .burger{
+//         @media(min-width: 768px) {
+//             display: none;
+//         }
+//     }
+// `
+
+function Navbar() {
+    // adding the states 
+    const [isActive, setIsActive] = useState(false);
+    //add the active class
+    const toggleActiveClass = () => {
+      setIsActive(!isActive);
+    };
+    //clean up function to remove the active class
+    const removeActive = () => {
+      setIsActive(false)
     }
     return (
-        <>
-        <NavContainer>
-            <h2>Navbar <span>Responsive</span></h2>
-            <ul className={`links ${clicked ? 'active': ''}`}>
-                <li><a href='/'>Home</a></li>
-                <li><a href='/'>Galeria Virtual</a></li>
-                <li><a href='/'>Sube tu Meme</a></li>
-                <li><a href='/'>Sobre Nosotros</a></li>
-                <li><a href='/'>Creadores</a></li>
-                <li><a href='/'>Contacto</a></li>
+      <div className="App">
+        <header className="App-header">
+          <nav className={`${styles.navbar}`}>
+            {/* logo */}
+            <a href='#home' className={`${styles.logo}`}>Dev. </a>
+            <ul className={`${styles.navMenu} ${isActive ? styles.active : ''}`}>
+              <li onClick={removeActive}>
+                <a href='#home' className={`${styles.navLink}`}>Home</a>
+              </li>
+              <li onClick={removeActive}>
+                <a href='#home' className={`${styles.navLink}`}>Catalog</a>
+              </li>
+              <li onClick={removeActive}>
+                <a href='#home' className={`${styles.navLink}`}>All products</a>
+              </li>
+              <li onClick={removeActive}>
+                <a href='#home' className={`${styles.navLink}`}>Contact</a>
+              </li>
             </ul>
-            <div className='burger'>
-                <BurgerButton clicked={clicked} handleClick={handleClick} />
+            <div className={`${styles.hamburger} ${isActive ? styles.active : ''}`}  onClick={toggleActiveClass}>
+              <span className={`${styles.bar}`}></span>
+              <span className={`${styles.bar}`}></span>
+              <span className={`${styles.bar}`}></span>
             </div>
-            
-        </NavContainer>
-        </>
-    )
-}
-
-export default Nav
-
-const NavContainer = styled.nav`
-    styled.h2{
-        color: white;
-        font-weight: 400;
-        span{
-            font-weight: bold;
-        }
-    }
-    padding: .4rem;
-    background-color: #333;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    a{
-        color: white;
-        text-text-decoration: none;
-        margin-right: 1rem;
-    }
-    .links{
-        position: absolute;
-        top: -700px;
-        left: -200px;
-        margin-left: auto;
-        margin-right: auto;
-        text-align: center;
-        a{
-            color: black;
-            font-size: 2rem;
-            display: block;
-        }
-        @media(min-width: 768px){
-            position: initial;
-            margin: 0;
-            a{
-                font-size: 1rem;
-                color: white;
-                display:inline;
-
-            }
-        }
-
-    }
-
-    .links.acive{
-        width: 100%;
-        display: block;
-        position: absolute;
-        margin-left: auto;
-        margin-right: auto;
-        top:30%;
-        left: 0;
-        right: 0;
-        text-align: center;
-    }
-    .burger{
-        @media(min-width: 768px) {
-            display: none;
-        }
-    }
-`
+          </nav>
+        </header>
+      </div>
+    );
+  }
+  export default Navbar;
+  ;
